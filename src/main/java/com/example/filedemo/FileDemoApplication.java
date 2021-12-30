8bdc3826e4235aaf03f7860f72a9d6f817854417
Anthony <18110175@student.hcmute.edu.vn>
2021-12-31 02:17:01 +0700

    add all

--- src/main/java/com/example/filedemo/FileDemoApplication.java
+++ src/main/java/com/example/filedemo/FileDemoApplication.java
@@ -4,12 +4,13 @@ import com.example.filedemo.property.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
 		FileStorageProperties.class
 })
-public class FileDemoApplication {
+public class FileDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FileDemoApplication.class, args);
